f1ab376a8882e55064eb400b634d1b48014ba3e1
michael <michael.lindfors@hotmail.com>
2018-12-12 09:58:50 +0200

    popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,70 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Button,Grid,Row,Col,Navbar,Nav,NavItem,NavDropdown,MenuItem } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCamera } from '@fortawesome/free-solid-svg-icons'
+import React, { Component } from "react";
+import "./App.css";
+import {
+  Button,
+  Grid,
+  Row,
+  Col,
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem,
+  DropdownButton
+} from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+
+import Popup from "reactjs-popup";
 
 const imgStyle = {
-  width : "100%"
+  width: "100%"
 };
 
 const gridItem = {
-  margin : "20px auto",
-  float: "none",
+  margin: "20px auto",
+  float: "none"
 };
 
-let userPosts = [{author: "nan",
-url: "https://www.gettyimages.fi/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg",
-title:"My First Post",
-createdAt:"Today",
-description:"This was my first post"},
-{author: "Wibly2",
-url: "https://www.gettyimages.fi/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg",
-title:"My second Post",
-createdAt:"Yesterday",
-description:"This was my second post"},
-{author: "kariez",
-url: "https://www.gettyimages.fi/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg",
-title:"My worst Post",
-createdAt:"today",
-description:"Dont"}]
+let userPosts = [
+  {
+    author: "nan",
+    url:
+      "https://www.gettyimages.fi/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg",
+    title: "My First Post",
+    createdAt: "Today",
+    description: "This was my first post"
+  },
+  {
+    author: "Wibly2",
+    url:
+      "https://www.gettyimages.fi/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg",
+    title: "My second Post",
+    createdAt: "Yesterday",
+    description: "This was my second post"
+  },
+  {
+    author: "kariez",
+    url:
+      "https://www.gettyimages.fi/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg",
+    title: "My worst Post",
+    createdAt: "today",
+    description: "Dont"
+  }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <header>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
-      </header>
+        <header>
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+            integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+            crossorigin="anonymous"
+          />
+        </header>
         <NAVBAR />
         <Posts items={userPosts} />
       </div>
@@ -47,7 +76,7 @@ class Posts extends Component {
   constructor(props) {
     super(props);
   }
-  Item () {
+  Item() {
     let items = [];
     for (var i = this.props.items.length - 1; i >= 0; i--) {
       items.push(
@@ -55,45 +84,76 @@ class Posts extends Component {
           <h3>{this.props.items[i].title}</h3>
           <img style={imgStyle} src={this.props.items[i].url} />
           <p>{this.props.items[i].description}</p>
-          <p>by <a href="#">{this.props.items[i].author}</a></p>
+          <p>
+            by <a href="#">{this.props.items[i].author}</a>
+          </p>
           <p>{this.props.items[i].createdAt}</p>
-        </Col>);
+        </Col>
+      );
       if (i != 0) {
-        items.push(<hr/>);
+        items.push(<hr />);
       }
     }
     return items;
   }
 
   render() {
-    return(
+    return (
       <Grid>
-        <Row className="show-grid">
-          {this.Item()}
-        </Row>
+        <Row className="show-grid">{this.Item()}</Row>
       </Grid>
     );
   }
 }
 
 class NAVBAR extends Component {
- render(){
-   return(
-     <Navbar inverse fixedTop >
-       <Grid style={{color: "#fff"}}>
-         <Row className="show-grid">
-           <Col xs={4} sm={4} md={4}>
-             <Nav pullLeft>
-               <FontAwesomeIcon href="#brand" icon={faCamera} style={{"fontSize" : "20px", "textAlign" : "left"}} />
-             </Nav>
-           </Col>
-           <Col xs={4} sm={4} md={4}>
-             <p style={{"fontSize" : "20px"}}>InstaClone</p>
-           </Col>
-         </Row>
-       </Grid>
-     </Navbar>
-   );
+  render() {
+    return (
+      <Navbar inverse fixedTop collapseOnSelect>
+        <Grid style={{ color: "#fff" }}>
+          <Row className="show-grid">
+            <Col xs={6} sm={6} md={6} style={{ textAlign: "left" }}>
+              <Popup
+                trigger={
+                  <FontAwesomeIcon
+                    href="#brand"
+                    id="basic-nav-dropdown"
+                    icon={faCamera}
+                    style={{ fontSize: "30px", marginTop: "10px" }}
+                  />
+                }
+                modal
+                closeOnDocumentClick>
+                <table>
+                  <tr>
+                    <td>
+                      <input type="text" placeholder="Image Url" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input type="text" placeholder="Title" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input type="text" placeholder="Description" />
+                    </td>
+                  </tr>
+                </table>
+              </Popup>
+            </Col>
+            <Col xs={6} sm={6} md={6} style={{ textAlign: "right" }}>
+              <a href="#" style={{ color: "white", textDecoration: "none" }}>
+                <p style={{ fontSize: "30px", marginTop: "10px" }}>
+                  InstaClone
+                </p>
+              </a>
+            </Col>
+          </Row>
+        </Grid>
+      </Navbar>
+    );
   }
 }
 
