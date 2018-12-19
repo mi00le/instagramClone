8c66f485043eeb2ef90687cb30661c661024f272
michael <michael.lindfors@hotmail.com>
2018-12-19 13:31:18 +0200

    popup

--- frontend/src/Components/Navbar/index.js
+++ frontend/src/Components/Navbar/index.js
@@ -27,59 +27,73 @@ export default class Navbar extends React.Component {
                 modal
                 closeOnDocumentClick
               >
-                <form className="form-style">
+                <div className="form-container">
                   <div className="row">
-                    <div className="col">
+                    <div className="col-25">
+                      <label>Image</label>
+                    </div>
+                    <div className="col-75">
                       <input
                         type="text"
                         id="imgUrl"
-                        className="form-control input-style"
-                        placeholder="Image Url"
-                        autoFocus
-                        required
+                        className="form-control"
+                        placeholder="Enter image url.."
                       />
-                    </div>
-                    <div className="col">
-                      <input
-                        type="text"
-                        className="form-control input-style"
-                        id="title"
-                        placeholder="Title"
-                        required
-                      />
-                    </div>
-                    <div className="col">
-                      <input
-                        type="text"
-                        className="form-control input-style"
-                        id="tags"
-                        placeholder="Tags"
-                        required
-                      />
-                    </div>
-                    <div className="col text-fix">
-                      <textarea
-                        className="form-control input-style"
-                        id="desc"
-                        placeholder="Description"
-                        required
-                      />
-                    </div>
-                    <div className="col">
-                      <button
-                        type="button"
-                        id="btn"
-                        className="form-control btn-primary input-style"
-                        onClick={this.props.handler}
-                      >
-                        Upload
-                      </button>
                     </div>
                   </div>
-                </form>
+                  <div className="row">
+                    <div className="col-25">
+                      <label>Title</label>
+                    </div>
+                    <div className="col-75">
+                      <input
+                        type="text"
+                        id="title"
+                        className="form-control"
+                        placeholder="Enter a title.."
+                      />
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-25">
+                      <label>Tags</label>
+                    </div>
+                    <div className="col-75">
+                      <input
+                        type="text"
+                        id="tags"
+                        className="form-control"
+                        placeholder="Enter tags.."
+                      />
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-25">
+                      <label>Subject</label>
+                    </div>
+                    <div className="col-75">
+                      <textarea
+                        id="desc"
+                        className="form-control"
+                        name="subject"
+                        placeholder="Write something.."
+                        style={{ height: "200px" }}
+                      />
+                    </div>
+                  </div>
+                  <div className="row">
+                    <button
+                      id="btn"
+                      className="form-control btn-primary"
+                      onClick={this.props.handler} 
+                    >
+                      Upload
+                    </button>
+                  </div>
+                </div>
               </Popup>
             </Col>
-            <Col xs={4}className="col-right">
+            <Col xs={4} className="col-right">
               <a href="#top" className="insta-clone">
                 <p className="insta-clone disable-select ">InstaClone</p>
               </a>
